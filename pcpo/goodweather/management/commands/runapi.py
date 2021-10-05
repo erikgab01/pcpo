c63baaa88a5e76107640b0c4de5b78495550e665
Tay Jen <SuperMan1223Norm@gmail.com>
2021-10-05 23:20:13 +0500

    smth changed I feel it

--- pcpo/goodweather/management/commands/runapi.py
+++ pcpo/goodweather/management/commands/runapi.py
@@ -1,48 +1,100 @@
 import requests
+from datetime import datetime
 
 from django.core.management.base import BaseCommand
 from goodweather.models import GoodDay
 
 
 class Command(BaseCommand):
-	def handle(self, *args, **options):
-		app_id = "27e3431f680b5d8be99fbbeed12e2dde"
-		# Coordinates (Ufa)
-		lat = 54.775
-		lon = 56.0375
-		# To get rid of extra data
-		exclude = "current,minutely,hourly"
-		# To get temperature in Celcius
-		units = 'metric'
-		# Additional args
-		city_id = 479561 	# Ufa
-		mode = 'json'	# Specifies output file
-		city = "Ufa,RU"
-		# Making request
-		try:
-			# This'll return weather forecast for 7 days ahead
-			res2 = requests.get(
-				"https://api.openweathermap.org/data/2.5/onecall",
-				params={'lat': lat, 'lon': lon, 'appid': app_id, 'exclude': exclude, 'units': units}
-			)
-			data = res2.json()
-			# Saving data into model objects
-			for number, day in enumerate(data['daily']):
-				day_time = str(day['dt'])
-				# If day wasn't saved before
-				if not GoodDay.objects.filter(date=day_time).exists():
-					good_day = GoodDay()
-					good_day.temp = day['temp']['day']
-					good_day.temp_feels_like = day['feels_like']['day']
-					good_day.humidity = day['humidity']
-					good_day.pressure = day['pressure']
-					good_day.wind_speed = day['wind_speed']
-					good_day.weather = day['weather'][0]['main']
-					good_day.date = day_time
-					good_day.save()
-					print(f"Saved {number}")
-				else:
-					print("not saved")
-		except Exception as e:
-			print("Exception: ", e)
-			pass
+    def handle(self, *args, **options):
+        data = self.get_data()
+        days = self.optimize_data(data)
+
+        for day_num in days:
+            # If the day passes according to the criteria, then it is saved in the database
+            if self.check_is_good(days[day_num]):
+                self.save_day(day_num, days[day_num])
+
+    def get_data(self):
+        app_id = "27e3431f680b5d8be99fbbeed12e2dde"
+        # Coordinates (Ufa)
+        lat = 54.775
+        lon = 56.0375
+        # To get rid of extra data
+        exclude = "current,minutely,hourly"
+        # To get temperature in Celcius
+        units = 'metric'
+        data = None
+        # Making request
+        try:
+            # This'll return weather forecast for 7 days ahead
+            res2 = requests.get(
+                "https://api.openweathermap.org/data/2.5/onecall",
+                params={'lat': lat, 'lon': lon, 'appid': app_id, 'exclude': exclude, 'units': units}
+            )
+            data = res2.json()
+        except Exception as e:
+            print("Exception: ", e)
+            pass
+
+        return data
+
+    def optimize_data(self, data):
+        days = dict(zip(range(8), data))
+        for day in days:
+            # Current day
+            current_day = days[day]
+            # Optimize the dictionary view
+            # current_day['temp_min'] = current_day['temp']['min']
+            current_day['temp_max'] = current_day['temp']['max']
+            current_day['feels_like_day'] = current_day['feels_like']['day']
+            current_day['weather_main'] = current_day['weather'][0]['main']
+            current_day['weather_desc'] = current_day['weather'][0]['description']
+            # Delete unimportant information
+            del current_day['sunrise'], \
+                current_day['sunset'], \
+                current_day['moonrise'], \
+                current_day['moonset'], \
+                current_day['moon_phase'], \
+                current_day['clouds'], \
+                current_day['wind_deg'], \
+                current_day['wind_gust'], \
+                current_day['temp'], \
+                current_day['feels_like'], \
+                current_day['weather']
+            # Transform seconds to Date in correct format
+            current_day['dt'] = datetime.fromtimestamp(current_day['dt']).strftime('%B %d, %Y')
+            print(f'Day {day}: {days[day]}')
+        return days
+
+    def check_is_good(self, day):
+        return (day['temp_min'] >= -3) and (day['temp_max'] <= 15) \
+               and (day['feels_like_day'] >= -3) and (day['feels_like_day'] <= 12) \
+               and (day['pressure'] >= 980) and (day['pressure'] <= 1036) \
+               and (day['humidity'] >= 40) and (day['humidity'] <= 60) \
+               and (day['dew_point'] <= 55) and (day['wind_speed'] <= 14) \
+               and (day['uvi'] <= 2) and (day['pop'] <= 50) and 'rain' not in day
+
+    def save_day(self, day_num, day):
+        # Saving data into model objects
+        day_time = day['dt']
+        # If day wasn't saved before
+        if not GoodDay.objects.filter(date=day_time).exists():
+            good_day = GoodDay()
+            good_day.date = day_time
+            good_day.temp_min = day['temp_min']
+            good_day.temp_min = day['temp_max']
+            good_day.feels_like_day = day['feels_like_day']
+            good_day.pressure = day['pressure']
+            good_day.humidity = day['humidity']
+            good_day.dew_point = day['dew_point']
+            good_day.wind_speed = day['wind_speed']
+            good_day.uvi = day['uvi']
+            good_day.pop = day['pop']
+            good_day.weather = day['weather_desc']
+            good_day.save()
+            print(f"Saved {day_num}")
+        else:
+            print("not saved")
+
+        return True
--- pcpo/goodweather/models.py
+++ pcpo/goodweather/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 
 class GoodDay(models.Model):
-	temp = models.FloatField(verbose_name='temp')
-	temp_feels_like = models.FloatField(verbose_name='temp_feels_like', null=True, blank=True)
-	humidity = models.IntegerField(verbose_name='humidity', null=True, blank=True)
-	pressure = models.IntegerField(verbose_name='pressure', null=True, blank=True)
-	wind_speed = models.FloatField(verbose_name='wind_speed', null=True, blank=True)
+	date = models.CharField(max_length=40, verbose_name='date', null=True, blank=True)
 	weather = models.CharField(max_length=20, verbose_name='weather', null=True, blank=True)
-	date = models.CharField(max_length=40, null=True, blank=True)
+
+	temp_min = models.FloatField(verbose_name='temp_min')
+	temp_max = models.FloatField(verbose_name='temp_max')
+	feels_like_day = models.FloatField(verbose_name='feels_like_day', null=True, blank=True)
+	pressure = models.IntegerField(verbose_name='pressure', null=True, blank=True)
+	humidity = models.IntegerField(verbose_name='humidity', null=True, blank=True)
+	dew_point = models.FloatField(verbose_name='dew_point', null=True, blank=True)
+	wind_speed = models.FloatField(verbose_name='wind_speed', null=True, blank=True)
+	uvi = models.FloatField(verbose_name='uvi', null=True, blank=True)
+	pop = models.IntegerField(verbose_name='pop', null=True, blank=True)
 	
 	def __str__(self):
 		return self.weather
@@ -17,4 +22,4 @@ class GoodDay(models.Model):
 	class Meta:
 		verbose_name_plural = 'GoodDays'
 		verbose_name = 'GoodDay'
-		ordering = ['-temp']
+		ordering = ['-date']
